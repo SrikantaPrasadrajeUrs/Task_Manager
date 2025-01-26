dc240e72d1ed12c34b6668b95964ce2ea2cb90b8
SrikantaPrasadrajeUrs <srikantaprasadrajeurs456@gmail.com>
2025-01-26 09:13:47 +0530

    post gress db setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express"
 import authRouter from "./routes/auth";
+import dotenv from "dotenv"
+
+dotenv.config();
+export default process.env.secret;
 
 const app = express();
-
-app.use("/auth",authRouter)
-
 app.use(express.json())
+app.use("/auth",authRouter)
 
 app.get("/",(req,res)=>{
     res.send("welcome to my app!");
